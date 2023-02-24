eff52d175c9eaf539cf74776f923eb573daf6507
Richard A. Israel <115384415+Rikpabi@users.noreply.github.com>
2023-02-24 18:28:50 +0100

    skill.h

--- skill.h
+++ skill.h
@@ -17,12 +17,12 @@
 "	Change the shell working directory.\n\n"\
 "	If no argument is given to cd the command will be interpreted\n"\
 "	as cd $HOME.\n"\
-"	if the argumenthelp is - (cd -), the command will be interpreted\n"\
+"	if the argument help is - (cd -), the command will be interpreted\n"\
 "	as cd $OLDPWD.\n\n"
 
 #define HELP_EXIT_MSG "exit=\n"\
 "exit:\texit [STATUS]\n\n"\
-"	Exit of the simple-shell.\n\n"\
+"	Exits the simple-shell.\n\n"\
 "	Exits the shell with a status of N.  If N is omitted, the exit status\n"\
 "	is that of the last command executed.\n\n"
 
@@ -40,16 +40,16 @@
 #define HELP_UNSETENV_MSG "unsetenv=\n"\
 "unsetenv:\tunsetenv VARIABLE\n\n"\
 "	The unsetenv function deletes one variable from the environment.\n\n"\
-"	Wen there are not correct numbers of arguments print error message.\n\n"
+"	When there are not correct numbers of arguments print error message.\n\n"
 
 #define HELP_MSG "help=\n"\
 "help:\thelp [BUILTIN_NAME]\n\n"\
-"	Display information about builtin commands.\n\n"\
+"	Displays information about builtin commands.\n\n"\
 "	Displays brief summaries of builtin commands.  If BUILTIN_NAME is\n"\
 "	specified, gives detailed help on all commands matching BUILTIN_NAME,\n"\
 "	otherwise the list of help topics is printed BUILTIN_NAME list.\n"\
 "	Arguments:\n\n"\
-"	BUILTIN_NAME specifiying a help topic.\n\n"\
+"	BUILTIN_NAME specifying a help topic.\n\n"\
 "	cd\t[dir]\n"\
 "	exit\t[status]\n"\
 "	env\n"\
